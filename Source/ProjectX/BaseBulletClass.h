e4c05cc2678acb868ac99caa0380891a38a767f8
ProjectX <sebastianwin93@hotmail.com>
2018-07-26 16:18:41 +0200

    update

--- Source/ProjectX/BaseBulletClass.h
+++ Source/ProjectX/BaseBulletClass.h
@@ -23,16 +23,15 @@ public:
    
     UProjectileMovementComponent *bulletMovement;
     USphereComponent *bulletVisuals;
-    
-    UPROPERTY(EditAnywhere);
-    UStaticMeshComponent *mesh;
+     
     
 protected:
     
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+public:
+    
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
     
